33a7c32c7ac656e540122eed24aae1135ec328b9
SaintRelion <saintrelion@gmail.com>
2026-03-01 17:55:46 +0800

    v3

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
+echo "Applying migrations..."
+python manage.py migrate --noinput
+
 # # Start Celery worker and beat in background
 # celery -A core worker -l info &
 # celery -A core beat -l info &
 
 # Start main API
+echo "Starting server..."
 exec uvicorn core.wsgi:application --host 0.0.0.0 --port 8000
