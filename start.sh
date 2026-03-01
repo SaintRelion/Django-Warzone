e489118b03229e09ed44ca24fd362852de427c2e
SaintRelion <saintrelion@gmail.com>
2026-03-01 17:05:31 +0800

    v9

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-# Start Celery worker and beat in background
-celery -A core worker -l info &
-celery -A core beat -l info &
+# # Start Celery worker and beat in background
+# celery -A core worker -l info &
+# celery -A core beat -l info &
 
 # Start main API
 exec uvicorn core.wsgi:application --host 0.0.0.0 --port 8000
