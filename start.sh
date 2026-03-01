4756e12a4362cde2312434c4c1752567d11d1228
SaintRelion <saintrelion@gmail.com>
2026-03-01 13:48:50 +0800

    updated start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,9 @@ set -e
 # Apply migrations
 python manage.py migrate --noinput
 
-# (Optional) collect static
-python manage.py collectstatic --noinput
-
 # Start Celery worker and beat in background
 celery -A core worker -l info &
 celery -A core beat -l info &
 
 # Start main API
-exec uvicorn core.wsgi:application -b 0.0.0.0:4005
+exec uvicorn core.wsgi:application -b 0.0.0.0:8000
